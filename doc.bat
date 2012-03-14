33521f5129bd8b3023bf5abc5a7a6afabae9a186
Frederic Bonnet <fredericbonnet@free.fr>
2012-03-14 16:52:15 +0100

    Converted project to Visual Studio 2010 Improved doc generation with dedicated targets in place of batch file

--- doc.bat
+++
@@ -1,7 +0,0 @@
-echo     Internal documentation:
-if not exist ..\doc\internal mkdir ..\doc\internal
-perl C:\Dev\NaturalDocs-1.51\NaturalDocs -i . -o HTML ../doc/internal -p InternalDocs
-
-echo     Public documentation:
-if not exist ..\doc\public mkdir ..\doc\public
-perl C:\Dev\NaturalDocs-1.51\NaturalDocs -i . -o HTML ../doc/public -p PublicDocs
